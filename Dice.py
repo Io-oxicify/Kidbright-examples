6d8f19f7de409fa0de18094cbcf7c61393222895
Oxic <pipatalt@gmail.com>
2026-03-27 20:29:54 +0700

    fixed the filesystem hell

--- Dice.py
+++
@@ -1,9 +0,0 @@
-import random
-import display
-
-display.scroll("Ready")
-while True:
-    input("enter to roll the dice..")
-    r = random.randint(1, 6)
-    print("rolled a", r)
-    display.scroll(r)
--- dice_reset.py
+++
@@ -1,8 +0,0 @@
-import display
-import random
-import time
-
-r = random.randint(1, 6)
-while True:
-    display.scroll(r)
-    time.sleep(0.5)
